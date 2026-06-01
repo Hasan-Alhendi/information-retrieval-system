bbfe2474bee2a2da234d113ca0810e318026ae25
Hasan Alhendi <72380459+Hasan-Alhendi@users.noreply.github.com>
2026-06-01 14:39:40 +0300

    feat: add refine query use case placeholder

---
+++ app/application/use_cases/refine_query.py
@@ -0,0 +1,12 @@
+"""Refine query use case."""
+
+
+class RefineQueryUseCase:
+    """Use case for query refinement."""
+
+    def execute(self, query: str) -> str:
+        """Return a refined version of the query.
+
+        A full implementation will be added when query refinement is migrated.
+        """
+        return query.strip()
