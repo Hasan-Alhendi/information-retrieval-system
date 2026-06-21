82bc10cd13a0d83ecf2b3f16c8db60c5e6b83478
Hasan Alhendi <72380459+Hasan-Alhendi@users.noreply.github.com>
2026-06-21 09:54:22 +0300

    remove automatic clustering model from config

--- app/config.py
+++ app/config.py
@@ -7,7 +7,6 @@ STORAGE_DIR = PROJECT_ROOT / "storage"
 DATASETS_DIR = STORAGE_DIR / "datasets"
 INDEXES_DIR = STORAGE_DIR / "indexes"
 VECTOR_STORES_DIR = STORAGE_DIR / "vector_stores"
-CLUSTERS_DIR = STORAGE_DIR / "clusters"
 EVALUATION_DIR = STORAGE_DIR / "evaluation"
 
 DEFAULT_TOP_K = 10
@@ -18,7 +17,6 @@ SUPPORTED_RETRIEVAL_MODELS = (
     "tfidf",
     "bm25",
     "embedding",
-    "embedding_clustered",
     "embedding_guided_categories",
     "hybrid_serial",
     "hybrid_parallel",
